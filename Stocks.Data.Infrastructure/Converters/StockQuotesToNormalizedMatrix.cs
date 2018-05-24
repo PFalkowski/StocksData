11c9ed27000fedbc5d1ad9319e534990a01576a5
Piotr Falkowski <falcon777@wp.pl>
2018-05-25 01:31:23 +0200

    add note

--- Stocks.Data.Infrastructure/Converters/StockQuotesToNormalizedMatrix.cs
+++ Stocks.Data.Infrastructure/Converters/StockQuotesToNormalizedMatrix.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace Stocks.Data.Infrastructure.Converters
 {
-    class StockQuotesToNormalizedMatrix
+    public class StockQuotesToNormalizedMatrix
     {
+        // TODO: implement
     }
 }
