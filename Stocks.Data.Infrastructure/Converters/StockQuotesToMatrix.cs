9271f3cf81b727aeb5cd699e0b5062f93aaad54f
Piotr Falkowski <falcon777@wp.pl>
2018-09-21 23:13:36 +0200

    Add valid comments

--- Stocks.Data.Infrastructure/Converters/StockQuotesToMatrix.cs
+++ Stocks.Data.Infrastructure/Converters/StockQuotesToMatrix.cs
@@ -6,6 +6,13 @@ namespace Stocks.Data.Infrastructure.Converters
 {
     public class StockQuotesToMatrix : IConverter<List<StockQuote>, double[][]>
     {
+        /// <summary>
+        /// <para>[0][0] = Open1,  [0][1] = High1,  [0][2] = Low1,  [0][3] = Close1,  [0][4] = Vol1</para>
+        /// <para>[1][0] = Open2,  [1][1] = High2,  [1][2] = Low2,  [1][3] = Close2,  [1][4] = Vol2</para>
+        /// <para>[2][0] = Open3,  [2][1] = High3,  [2][2] = Low3,  [2][3] = Close3,  [2][4] = Vol3</para>
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>new double[][] { sample1, sample2, sample3... }</returns>
         public double[][] Convert(List<StockQuote> input)
         {
             double[][] result = new double[input.Count][];
--- Stocks.Data.Infrastructure/Converters/StockQuotesToMatrixRotated.cs
+++ Stocks.Data.Infrastructure/Converters/StockQuotesToMatrixRotated.cs
@@ -6,6 +6,16 @@ namespace Stocks.Data.Infrastructure.Converters
 {
     public class StockQuotesToMatrixRotated : IConverter<List<StockQuote>, double[][]>
     {
+
+        /// <summary>
+        /// <para>[0][0] = Open1,  [0][1] = Open2,  [0][2] = Open3...</para>
+        /// <para>[1][0] = High1,  [1][1] = High2,  [1][2] = High3...</para>
+        /// <para>[2][0] = Low1,   [2][1] = Low2,   [2][2] = Low3...</para>
+        /// <para>[3][0] = Close1, [3][1] = Close2, [3][2] = Close3...</para>
+        /// <para>[4][0] = Vol1,   [4][1] = Vol2,   [4][2] = Vol3...</para>
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>new double[][] { opens, highs, lows, closes, vols }</returns>
         public double[][] Convert(List<StockQuote> input)
         {
             var opens = new double[input.Count];
