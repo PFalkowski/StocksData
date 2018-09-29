c0b42ff4f2230369a17b3a6e86acf478986b29a1
Piotr Falkowski <falcon777@wp.pl>
2018-09-29 20:47:19 +0200

    Update NuGets

---
+++ Stocks.Data.UnitTests.Services/StocksFileProviderTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Stocks.Data.UnitTests.Services
+{
+    public class StocksFileProviderTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
