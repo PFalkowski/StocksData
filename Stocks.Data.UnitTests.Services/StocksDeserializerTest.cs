6f0fe13c17c9f39f6e4e569ae21528a3065a6cbe
Piotr Falkowski <falcon777@wp.pl>
2018-10-09 21:13:15 +0200

    Go to .NET Core 2.1 in tests projects. Add unit test

--- Stocks.Data.UnitTests.Services/StocksDeserializerTest.cs
+++ Stocks.Data.UnitTests.Services/StocksDeserializerTest.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Text;
-using CsvHelper.Configuration;
-using NSubstitute;
-using Stocks.Data.Model;
-using Stocks.Data.Services;
+﻿using Stocks.Data.Services;
 using Stocks.Data.UnitTests.Services.Mocks;
+using System;
+using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Stocks.Data.UnitTests.Services
@@ -19,13 +17,23 @@ namespace Stocks.Data.UnitTests.Services
             Assert.Equal(System.Globalization.CultureInfo.InvariantCulture, tested.Culture);
         }
         [Fact]
-        public void Deserializer_WithValidParameters_Deserializes()
+        public void Deserialize_GivenValidParameters_Deserializes()
         {
             var mapMock = new MockStockClassMap();
             var tested = new StocksDeserializer(mapMock);
-            var cdProject = Encoding.UTF8.GetString( Properties.Resources.CDPROJEKT);
+            var cdProject = Encoding.UTF8.GetString(Properties.Resources.CDPROJEKT);
 
             var result = tested.Deserialize(cdProject);
+            Assert.True(result.Quotes.TrueForAll(q => q.Ticker.Equals("CDPROJEKT", StringComparison.InvariantCultureIgnoreCase)));
+        }
+        [Fact]
+        public async Task DeserializeAsync_GivenValidParameters_Deserializes()
+        {
+            var mapMock = new MockStockClassMap();
+            var tested = new StocksDeserializer(mapMock);
+            var cdProject = Encoding.UTF8.GetString(Properties.Resources.CDPROJEKT);
+
+            var result = await tested.DeserializeAsync(cdProject);
             Assert.True(result.Quotes.TrueForAll(q => q.Ticker.Equals("CDPROJEKT", StringComparison.InvariantCultureIgnoreCase)));
         }
     }
